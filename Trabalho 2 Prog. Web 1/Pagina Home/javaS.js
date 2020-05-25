60377e20d42474519ebce54ea725de34f19391fe
Martha Lanser Bloemer <tatalb@gmail.com>
2020-05-25 09:04:15 -0300

    inserindo imagens

---
+++ Trabalho 2 Prog. Web 1/Pagina Home/javaS.js
@@ -0,0 +1,10 @@
+function bordaAlterada(imagen) {
+    imagen.style.height = "250px"
+    imagen.style.width = "250px"
+    imagen.style.border = "4px #0066aa solid "
+}
+function bordaNormal(imagen) {
+    imagen.style.height = "150px"
+    imagen.style.width = "150px"
+    imagen.style.border = ""
+}
