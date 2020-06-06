4284292b5c263be4bd6b415534de83ed2fb1917e
Martha Lanser Bloemer <tatalb@gmail.com>
2020-06-06 18:01:50 -0300

    alteração do ajude para button
    corrigindo for e id do form que devem ser iguais
    adicionando fonte MULI

--- Trabalho 2 Prog. Web 1/Pagina Home/javaS.js
+++ Trabalho 2 Prog. Web 1/Pagina Home/javaS.js
@@ -8,3 +8,16 @@ function bordaNormal(imagen) {
     imagen.style.width = "150px"
     imagen.style.border = ""
 }
+function ajudeBlumenau() {
+    alert("Organização Guaxinim Melhor Que Capivara LTDA.\nCNPJ: 67.553.331/0001-20\nBanco: Banco do Brasil \nAgência: 3432-0\nC/C: 21934-5");
+}
+function ajudeMassaranduba() {
+    alert("Organização Guaxaranduba LTDA.\nCNPJ: 41.064.624/0001-50\nBanco: Caixa Econômica \nAgência: 0135-1\nC/C: 2526-7");
+}
+function ajudeRioDosCedros() {
+    alert("ONG Mattedixinim LTDA.\nCNPJ: 01.991.927/0001-01\nBanco: Viacredi \nAgência: 0101\nC/C: 987.223-55");
+}
+function ajudePomerode() {
+    alert("Waschbär Niedlicher Allesfresser LTDA.\nCNPJ: 91.532.874/0001-45\nBanco: Banco do Brasil \nAgência: 5548-7\nC/C: 68598-7");
+}
+
